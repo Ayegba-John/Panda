739139e88295e954109b23e435be9e54e6019785
Ayegba John <ayegbajohn@gmail.com>
2024-12-21 23:36:35 +0100

    footer added

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+const footer = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>
+            Pandar is a registered trademark of Pandar Resources Ltd with RC
+            1711476 as a duly registered legal entity in Nigeria, with the
+            Nigerian Corporate Affairs Commission. Any unauthorized
+            redistribution or reproduction of any copyrighted materials on this
+            website is strictly prohibited. Other product and company names are
+            trademarks of their respective owners. 1309 Coffeen Avenue STE 1200,
+            Sheridan, Wyoming, 82801.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default footer;
